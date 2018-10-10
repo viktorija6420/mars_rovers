a7bb4e1d4f549832ce520e26893e5ff1ed3f7ac8
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2018-10-10 09:59:56 -0400

    rovers_position defined, puts shows rover_x, rover_y and heading

--- main.rb
+++ main.rb
@@ -18,3 +18,26 @@ end
 
 position_plateau = Plateau.new(input_array[0], input_array[1])
 puts position_plateau.plateau_x_upper_right, position_plateau.plateau_y_upper_right
+
+class Rovers_position
+  def initialize (rover_x, rover_y, heading)
+    @rover_x = rover_x
+    @rover_y = rover_y
+    @heading = heading
+
+    def rover_x
+      @rover_x
+    end
+
+    def rover_y
+      @rover_y
+    end
+
+    def heading
+      @heading
+    end
+  end
+end
+
+rovers_position = Rovers_position.new(input_array[2], input_array[3], input_array[4])
+puts rovers_position.rover_x, rovers_position.rover_y, rovers_position.heading
